f3b24022381f66536d2b8fbb3f8c4af65cdb55ea
Topo81 <103972679+Topo81@users.noreply.github.com>
2022-08-11 20:22:55 -0300

    Create ex11.js

---
+++ ex11.js
@@ -0,0 +1,19 @@
+/*Crea un nuevo fichero JS que contenga las siguientes líneas
+- Una clase llamada "Estudiante" que tenga:
+- Una variable llamada nombre
+- Otra variable lista llamada asignaturas con 3 asignaturas: Javascript, HTML, CSS
+- Un método "obtenDatos" que devuelva un objeto con las propiedades nombre y asignaturas
+- Crea una nueva instancia de "Estudiante"
+- Haz la llamada al método obtenDatos */
+
+class Estudiante{
+    constructor(nombre,asignaturas){
+    this.nombre= nombre
+    this.asignaturas= ["Javascript","HTML","CSS"]}
+
+    obtenDatos(){
+        new Estudiante(this.nombre,this.asignaturas)
+    }
+}
+let otroestudiante= new Estudiante("Tomas",)
+otroestudiante.obtenDatos
